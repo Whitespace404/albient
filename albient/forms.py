1ee25caa4e81877729183a296b752d65fc559f43
Whitespace404 <rush2asharahul@gmail.com>
2024-07-20 21:51:36 +0530

    tried to do stuff

--- albient/forms.py
+++ albient/forms.py
@@ -1,13 +1,22 @@
 from flask_wtf import FlaskForm
-
-from wtforms import (StringField, 
-                     SubmitField, 
-                     PasswordField, 
-                     SelectField, 
-                     TextAreaField)
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Length, EqualTo, DataRequired
 
 
 class CreateUserForm(FlaskForm):
-    username = StringField("Username")
-    password = PasswordField("Password")
-    confirm_password = PasswordField("Confirm Password")
+    username = StringField("Username", validators=[Length(max=64)])
+    display_name = StringField("Display Name: ", validators=[Length(max=64)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(max=64)])
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[DataRequired(), EqualTo("password", "Passwords do not match")],
+    )
+
+    submit = SubmitField()
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[Length(max=64)])
+    password = PasswordField("Password", validators=[Length(max=64)])
+
+    submit = SubmitField()
--- albient/routes.py
+++ albient/routes.py
@@ -1,13 +1,18 @@
-from albient import app
 from flask import render_template
+
+from albient import app
+from albient.forms import LoginForm, CreateUserForm
 
 
 @app.route("/home")
 @app.route("/")
 def home():
     ish = [
-        ["What is 1+1", "I am not having the clarity in the maths you know what is 1+1 is"],
-        ["how to do crosss aldol addiotn", 'ethanal']
+        [
+            "What is 1+1",
+            "I am not having the clarity in the maths you know what is 1+1 is",
+        ],
+        ["how to do crosss aldol addiotn", "ethanal"],
     ]
     return render_template("home.html", questions=ish)
 
@@ -15,3 +20,12 @@ def home():
 @app.route("/login")
 def login():
     return "<h1> login </h1>"
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = CreateUserForm()
+    if form.validate_on_submit:
+        print(form.username.data)
+        print(form.password.data)
+    return render_template("register.html", form=form)

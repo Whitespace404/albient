45eba3fd35f150aec770b49acc0b06cd41456e4f
Whitespace404 <rush2asharahul@gmail.com>
2024-07-20 21:04:48 +0530

    lab srivaths changes

--- albient/__init__.py
+++ albient/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "m15mtwtmwilhsmm_aw3W5akn6aGpZfTzi"
+app.config["SECRET_KEY"] = "m15mtwtmwishanvip10lhsmm_aw3W5akn6aGpZf3TIsh"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 
 db = SQLAlchemy(app)
--- albient/routes.py
+++ albient/routes.py
@@ -5,7 +5,11 @@ from flask import render_template
 @app.route("/home")
 @app.route("/")
 def home():
-    return render_template("home.html")
+    ish = [
+        ["What is 1+1", "I am not having the clarity in the maths you know what is 1+1 is"],
+        ["how to do crosss aldol addiotn", 'ethanal']
+    ]
+    return render_template("home.html", questions=ish)
 
 
 @app.route("/login")

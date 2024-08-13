d27d5f9f3c2eb659b73959cb77c1c54449224217
Whitespace404 <rush2asharahul@gmail.com>
2024-08-13 11:23:53 +0530

    add users to db on init

--- init_db.py
+++ init_db.py
@@ -1,4 +1,27 @@
 from albient import app, db
+from albient.models import User
 
 with app.app_context():
     db.create_all()
+
+    user = User(
+            username="flyingseverus",
+            email="sev@gmail.com",
+            password="power",
+            display_name="Sev",
+        )
+    user1 = User(
+            username="pittetan",
+            email="pittan@gmail.com",
+            password="power",
+            display_name="Pittan",
+        )
+    user2 = User(
+            username="srivaths",
+            email="srivaths@gmail.com",
+            password="power",
+            display_name="Srivaths",
+        )
+
+    db.session.add_all([user, user1, user2])
+    db.session.commit()
